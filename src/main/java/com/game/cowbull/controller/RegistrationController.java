c3909d75d705d591e20c9c60c022165eb49d8e01
Иван <ivan.jojojos@yandex.ru>
2020-02-25 12:40:28 +0400

    add password encoder

--- src/main/java/com/game/cowbull/controller/RegistrationController.java
+++ src/main/java/com/game/cowbull/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import com.game.cowbull.entity.Role;
 import com.game.cowbull.entity.User;
 import com.game.cowbull.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,8 @@ import java.util.Map;
 public class RegistrationController {
     @Autowired
     private UserRepo userRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping("/registration")
     public String registration() {
@@ -32,6 +35,7 @@ public class RegistrationController {
 
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
         return "redirect:/login";
     }
